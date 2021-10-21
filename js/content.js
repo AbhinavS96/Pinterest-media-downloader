6fb8ed8fbebee2cba846700ce77f6fc7300db72f
Abhinav <abhinavsidharth@gmail.com>
2021-10-21 20:26:22 +0530

    changed the detection method to pick the largest image

--- js/content.js
+++ js/content.js
@@ -1,15 +1,21 @@
-chrome.runtime.sendMessage({"todo":"showPageAction"});
+chrome.runtime.sendMessage({ "todo": "showPageAction" });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>{
-	if(request.todo == "openNewTab"){
-		window.open(document.querySelector('.zI7 img').src, '_blank');
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	//identify the largest image from the page. There could be two image links. The last one is usually the largest.
+	let imageArray = document.querySelectorAll('[data-test-id=closeup-image] .zI7 img');
+	let mainImage = imageArray[imageArray.length - 1];
+
+	//choose what to do depending on the button clicked
+	if (request.todo == "openNewTab") {
+		window.open(mainImage.src, '_blank');
 	}
-	else if(request.todo == "saveImage"){
+	//using a hack to save the image. A link element for saving is added and clicked. Then it is removed.
+	else if (request.todo == "saveImage") {
 		let link = document.createElement('a');
 		link.href = '#';
 		link.target = '_blank';
-		link.download = document.querySelector('.zI7 img').src;
-		document.body.appendChild(link);	
+		link.download = mainImage.src;
+		document.body.appendChild(link);
 		link.click();
 		document.body.removeChild(link);
 	}
