7ae1920b27af3e348487aa9c9e4d464ee6ffcefa
Abhinav <abhinavsidharth@gmail.com>
2021-10-21 20:43:03 +0530

    added support for downloading videos as well

--- js/content.js
+++ js/content.js
@@ -2,7 +2,13 @@ chrome.runtime.sendMessage({ "todo": "showPageAction" });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	//identify the largest image from the page. There could be two image links. The last one is usually the largest.
+	//does not work for videos
 	let imageArray = document.querySelectorAll('[data-test-id=closeup-image] .zI7 img');
+	//if image array is empty then the page has a video
+	if (imageArray.length == 0) {
+		//using the same name for the variable to reuse the code. Only one video will get picked and stored in an array of length 1
+		imageArray = document.querySelectorAll('video');
+	}
 	let mainImage = imageArray[imageArray.length - 1];
 
 	//choose what to do depending on the button clicked
