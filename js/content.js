72f60cd9c6f64ddc2e6d66fa1c5006e9459d34da
AbhinavS96 <abhinavsidharth@gmail.com>
2021-11-12 22:55:37 +0530

    picked up images and videos from carousels. single images and videos are also picked up

--- js/content.js
+++ js/content.js
@@ -4,12 +4,37 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	
 	const data = JSON.parse(document.querySelector('#__PWS_DATA__').innerText)
 
+	//get the first key out from storypins. This is hard to guess
 	let storyKey = Object.keys(data.props.initialReduxState.storyPins)[0]
-	let pages = data.props.initialReduxState.storyPins[storyKey].pages
+	let storyPins = data.props.initialReduxState.storyPins
+	//check if the page has stories. Otherwise it is a single pic or video
+	if(storyPins[storyKey]){ console.log('test')
+		//get pages as an array 
+		let pages = storyPins[storyKey].pages
 
-	pages.forEach(page => {
-		console.log(page.blocks[0].image.images.originals.url)
-	});
+		//loop over pages and get all the images or videos
+		pages.forEach(page => {
+			let image = page.blocks[0].image;
+			if(image)
+				console.log(image.images.originals.url)
+			else
+				console.log(page.blocks[0].video.video_list.V_EXP3.url)
+		});
+	}
+	else{
+		//take a similar approach as in the if condition
+		let pinsKey = Object.keys(data.props.initialReduxState.pins)[0]
+		let pins = data.props.initialReduxState.pins[pinsKey]
+
+		//check if this is a video
+		if(pins.videos){
+			let videoKey = Object.keys(pins.videos.video_list)[0]
+			console.log(pins.videos.video_list[videoKey].url)
+		}
+		else{
+			console.log(pins.images.orig.url)
+		}
+	}
 	
 	if (request.todo == "openNewTab") {
 		window.open(mediaLink, '_blank');
