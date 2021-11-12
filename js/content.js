04527302fa887e007060a49a8efec4970bdfb3ab
AbhinavS96 <abhinavsidharth@gmail.com>
2021-11-12 12:00:32 +0530

    made a detection object to choose the selector from

--- js/content.js
+++ js/content.js
@@ -1,19 +1,37 @@
 chrome.runtime.sendMessage({ "todo": "showPageAction" });
 
+//Array of selectors for detection
+const selectorJSON = {
+	'[data-test-id=closeup-image] .zI7 img':'image',
+	'[data-test-id=pin-closeup-image] .zI7 img':'image',
+	'video':'video',
+	'div[role=img]':'backgroundImage'
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	//check if this selector exists on the page. otherwise another selector has to be used
-	let selector = '[data-test-id=closeup-image] .zI7 img';
-	if(document.querySelectorAll(selector).length == 0)
-		selector = '[data-test-id=pin-closeup-image] .zI7 img';
-	//identify the largest image from the page. There could be two image links. The last one is usually the largest.
-	//does not work for videos
-	let imageArray = document.querySelectorAll(selector);
-	//if image array is empty then the page has a video
-	if (imageArray.length == 0) {
-		//using the same name for the variable to reuse the code. Only one video will get picked and stored in an array of length 1
-		imageArray = document.querySelectorAll('video');
-	}
-	let mainImage = imageArray[imageArray.length - 1];
+	
+	let selector = ""
+	//try and check which selector would match
+	Object.keys(selectorJSON).forEach(sel =>{
+		if(document.querySelectorAll(sel).length > 0)
+			selector = sel
+			console.log(selector)
+	})
+	
+	// //check if this selector exists on the page. otherwise another selector has to be used
+	// let selector = '[data-test-id=closeup-image] .zI7 img';
+	// if(document.querySelectorAll(selector).length == 0)
+	// 	selector = '[data-test-id=pin-closeup-image] .zI7 img';
+	// //identify the largest image from the page. There could be two image links. The last one is usually the largest.
+	// //does not work for videos
+	// let imageArray = document.querySelectorAll(selector);
+	// //if image array is empty then the page has a video
+	// if (imageArray.length == 0) {
+	// 	//using the same name for the variable to reuse the code. Only one video will get picked and stored in an array of length 1
+	// 	imageArray = document.querySelectorAll('video');
+	// }
+	let mediaArray = document.querySelectorAll(selector);
+	let mainImage = mediaArray[mediaArray.length - 1];
 
 	//choose what to do depending on the button clicked
 	if (request.todo == "openNewTab") {
