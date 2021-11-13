812fb882393e45298944054fbebedef0a4951d54
AbhinavS96 <abhinavsidharth@gmail.com>
2021-11-13 15:48:57 +0530

    basic UI don

--- js/main.js
+++ js/main.js
@@ -3,11 +3,21 @@ window.onload = () =>{
 	chrome.tabs.query({active:true, currentWindow:true}, (tabs)=>{
 		chrome.tabs.sendMessage(tabs[0].id,	{todo: "getData"}, (res) => {
 			console.log(res[0])
-		})
-	})
-	document.getElementById('newTabButton').addEventListener("click", ()=>{
-		chrome.tabs.query({active:true, currentWindow:true}, (tabs)=>{
-			chrome.tabs.sendMessage(tabs[0].id,	{todo: "openNewTab"})
+			//create the download elements
+			res.forEach((element, index) => {
+				let li = document.createElement("li")
+				let button = document.createElement("button")
+				let img = document.createElement("img")
+				img.src = element.url
+				img.classList = "image"
+				button.innerText = "Download " + element.type
+				button.classList = "btn btn-outline-danger"
+				let text = document.createTextNode((index+1).toString())
+				li.appendChild(text)
+				li.appendChild(img)
+				li.appendChild(button)
+				document.getElementById("downloads").appendChild(li)
+			});
 		})
 	})
 
