bb6545254d0289ef6fb49cc598f596f75b98b4f4
AbhinavS96 <abhinavsidharth@gmail.com>
2022-01-09 11:50:54 +0530

    beta support added for collection pages

--- js/content.js
+++ js/content.js
@@ -17,16 +17,16 @@ const pinMediaArray = () => {
   const data = JSON.parse(document.querySelector("#__PWS_DATA__").textContent);
 
   //get the first key out from storypins. This is hard to guess
-  let storyKey = Object.keys(data.props.initialReduxState.storyPins)[0];
-  let storyPins = data.props.initialReduxState.storyPins;
+  const storyKey = Object.keys(data.props.initialReduxState.storyPins)[0];
+  const storyPins = data.props.initialReduxState.storyPins;
   //check if the page has stories. Otherwise it is a single pic or video
   if (storyPins[storyKey]) {
     //get pages as an array
-    let pages = storyPins[storyKey].pages;
+    const pages = storyPins[storyKey].pages;
 
     //loop over pages and get all the images or videos
     pages.forEach((page) => {
-      let image = page.blocks[0].image;
+      const image = page.blocks[0].image;
       if (image)
         response.push({
           imageURL: image.images.originals.url,
@@ -42,12 +42,12 @@ const pinMediaArray = () => {
     });
   } else {
     //take a similar approach as in the if condition
-    let pinsKey = Object.keys(data.props.initialReduxState.pins)[0];
-    let pins = data.props.initialReduxState.pins[pinsKey];
+    const pinsKey = Object.keys(data.props.initialReduxState.pins)[0];
+    const pins = data.props.initialReduxState.pins[pinsKey];
 
     //check if this is a video
     if (pins.videos) {
-      let videoKey = Object.keys(pins.videos.video_list)[0];
+      const videoKey = Object.keys(pins.videos.video_list)[0];
       response.push({
         imageURL: pins.images.orig.url,
         type: "video",
@@ -80,12 +80,33 @@ const collectionMediaArray = () => {
     feedKey
   ].data.forEach((item) => {
     if (item.videos) {
-      let videoKey = Object.keys(item.videos.video_list)[0];
-      console.log(videoKey);
+      const videoKey = Object.keys(item.videos.video_list)[0];
       response.push({
         imageURL: item.videos.video_list[videoKey].thumbnail,
         type: "video",
         downloadURL: item.videos.video_list[videoKey].url,
+      });
+    } else if (item.story_pin_data) {
+      item.story_pin_data.pages.forEach((page) => {
+        const image = page.blocks[0].image;
+        if (image)
+          response.push({
+            imageURL: image.images.originals.url,
+            type: "image",
+            downloadURL: image.images.originals.url,
+          });
+        else
+          response.push({
+            imageURL: page.blocks[0].video.video_list.V_EXP3.thumbnail,
+            type: "video",
+            downloadURL: page.blocks[0].video.video_list.V_EXP3.url,
+          });
+      });
+    } else if (item.images) {
+      response.push({
+        imageURL: item.images.orig.url,
+        type: "image",
+        downloadURL: item.images.orig.url,
       });
     }
   });
