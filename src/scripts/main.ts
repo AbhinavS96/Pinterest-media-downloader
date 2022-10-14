f0ec6ca1c9da9adaef6e24791a84bd6471100392
AbhinavS96 <abhinavsidharth@gmail.com>
2022-10-14 22:38:05 +0200

    migration done. pending cleanup

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,93 +1,101 @@
-// chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//   chrome.tabs.sendMessage(tabs[0].id, { todo: "getData" }, (res) => {
-//     if (res.length > 0) {
-//       //hide the error message
-//       document.querySelector("#errorMessage").hidden = true;
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  chrome.tabs.sendMessage(tabs[0].id, { todo: "getData" }, (res) => {
+    if (res.length > 0) {
+      //hide the error message
+      (document.querySelector("#errorMessage") as HTMLElement).hidden = true;
 
-//       //create the download elements
-//       res.forEach((element, index) => {
-//         const li = document.createElement("li");
-//         const button = document.createElement("button");
-//         const img = document.createElement("img");
-//         img.src = element.imageURL;
-//         img.classList = "image";
-//         img.loading = "lazy";
-//         const span = document.createElement("span");
-//         span.textContent = "Download " + element.type;
-//         const loader = document.createElement("div");
-//         loader.classList = "loader";
-//         loader.setAttribute("hidden", "true");
-//         button.appendChild(span);
-//         button.appendChild(loader);
-//         button.classList = "btn btn-outline-danger";
-//         const text = document.createTextNode((index + 1).toString() + ".");
-//         li.appendChild(text);
-//         li.appendChild(img);
-//         li.appendChild(button);
+      type DownloadResponse = {
+        imageURL: string;
+        type: string;
+        downloadURL: string;
+      };
 
-//         // add event listeners to the buttons
-//         document.getElementById("downloads").appendChild(li);
-//         button.addEventListener("click", () => {
-//           chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//             chrome.tabs.sendMessage(tabs[0].id, {
-//               todo: "saveImage",
-//               downloadURL: res[index].downloadURL,
-//               type: res[index].type,
-//               index: index,
-//             });
-//           });
-//         });
-//       });
+      //create the download elements
+      res.forEach((element: DownloadResponse, index: number) => {
+        const li = document.createElement("li");
+        const button = document.createElement("button");
+        const img = document.createElement("img");
+        img.src = element.imageURL;
+        img.classList.value = "image";
+        img.loading = "lazy";
+        const span = document.createElement("span");
+        span.textContent = "Download " + element.type;
+        const loader = document.createElement("div");
+        loader.classList.value = "loader";
+        loader.setAttribute("hidden", "true");
+        button.appendChild(span);
+        button.appendChild(loader);
+        button.classList.value = "btn btn-outline-danger";
+        const text = document.createTextNode((index + 1).toString() + ".");
+        li.appendChild(text);
+        li.appendChild(img);
+        li.appendChild(button);
 
-//       //for the save all button
-//       if (res.length == 1) {
-//         document.querySelector("#downloadAllContainer").hidden = true;
-//       } else {
-//         document.getElementById("saveButton").addEventListener("click", () => {
-//           chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//             chrome.tabs.sendMessage(tabs[0].id, {
-//               todo: "saveAllImages",
-//               downloadURLs: res.map((i) => i.downloadURL),
-//               index: res.length,
-//             });
-//           });
-//         });
-//       }
-//     } else {
-//       //remove the download container just for visual appeal
-//       document.querySelector("#downloadContainer").hidden = true;
-//       document.querySelector("#downloadAllContainer").hidden = true;
-//     }
-//   });
-// });
+        // add event listeners to the buttons
+        document.getElementById("downloads").appendChild(li);
+        button.addEventListener("click", () => {
+          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+              todo: "saveImage",
+              downloadURL: res[index].downloadURL,
+              type: res[index].type,
+              index: index,
+            });
+          });
+        });
+      });
 
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   //enable or disable the button based on the status of the download returned
-//   if (request.download) {
-//     document
-//       .querySelectorAll("button")
-//       [request.index].setAttribute("disabled", true);
-//     document
-//       .querySelectorAll("button")
-//       [request.index].querySelector(".loader").hidden = false;
-//     document
-//       .querySelectorAll("button")
-//       [request.index].querySelector("span").hidden = true;
-//   } else {
-//     document
-//       .querySelectorAll("button")
-//       [request.index].removeAttribute("disabled");
-//     document
-//       .querySelectorAll("button")
-//       [request.index].querySelector(".loader").hidden = true;
-//     document
-//       .querySelectorAll("button")
-//       [request.index].querySelector("span").hidden = false;
-//   }
-// });
+      //for the save all button
+      if (res.length == 1) {
+        (
+          document.querySelector("#downloadAllContainer") as HTMLElement
+        ).hidden = true;
+      } else {
+        document.getElementById("saveButton").addEventListener("click", () => {
+          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+              todo: "saveAllImages",
+              downloadURLs: res.map((i: DownloadResponse) => i.downloadURL),
+              index: res.length,
+            });
+          });
+        });
+      }
+    } else {
+      //remove the download container just for visual appeal
+      (document.querySelector("#downloadContainer") as HTMLElement).hidden =
+        true;
+      (document.querySelector("#downloadAllContainer") as HTMLElement).hidden =
+        true;
+    }
+  });
+});
 
-function test2(a: string) {
-  console.log(a);
-}
-
-test2("abc");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  //enable or disable the button based on the status of the download returned
+  if (request.download) {
+    document
+      .querySelectorAll("button")
+      [request.index].setAttribute("disabled", "true");
+    (
+      document
+        .querySelectorAll("button")
+        [request.index].querySelector(".loader") as HTMLElement
+    ).hidden = false;
+    document
+      .querySelectorAll("button")
+      [request.index].querySelector("span").hidden = true;
+  } else {
+    document
+      .querySelectorAll("button")
+      [request.index].removeAttribute("disabled");
+    (
+      document
+        .querySelectorAll("button")
+        [request.index].querySelector(".loader") as HTMLElement
+    ).hidden = true;
+    document
+      .querySelectorAll("button")
+      [request.index].querySelector("span").hidden = false;
+  }
+});
